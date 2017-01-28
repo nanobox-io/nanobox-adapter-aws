089594a3f80e7061ec1b7be011d55ccbed5b94ac
Tyler Flint <tylerflint@gmail.com>
2017-01-27 17:13:48 -0700

    Add ebs pricing to catalog estimate

--- lib/ec2/catalog.rb
+++ lib/ec2/catalog.rb
@@ -2,8 +2,14 @@ require 'faraday'
 
 module ::EC2::Catalog
 
+  ASSET_FILE = File.expand_path('../../assets/catalog.json', __FILE__)
+
   def self.fetch
-    
+    begin
+      ::File.read ASSET_FILE
+    rescue
+      nil
+    end
   end
   
   def self.generate
@@ -33,6 +39,9 @@ module ::EC2::Catalog
     # grab the ec2 instances pricing structure dump
     options = self.ec2_instances_info
     
+    # grab the ebs prices
+    ebs_prices = self.ebs_prices
+    
     # first setup the datacenter region structures (later, we'll convert to a list)
     dcs = {}
     
@@ -53,9 +62,17 @@ module ::EC2::Catalog
         family        = option['family']
         family_id     = family.downcase.gsub(/ /, '_')
         instance_type = option['instance_type']
-        vcpu          = option['vCPU']
-        memory        = option['memory']
-        hourly_price  = platform['linux']['ondemand'] rescue 'N/A'
+        cpus          = option['vCPU'].to_f / 4
+        memory        = (option['memory'] * 1024).to_i
+        disk          = calculate_disk(memory)
+        hourly_price  = platform['linux']['ondemand'].to_f rescue 'N/A'
+        monthly_price = hourly_price * 720 rescue 'N/A'
+        
+        # adjust the hourly/monthly prices to reflect disk prices
+        if hourly_price != 'N/A'
+          hourly_price = hourly_price + disk * ebs_prices[region][:hourly]
+          monthly_price = monthly_price + disk * ebs_prices[region][:monthly]
+        end
         
         # short-circuit if there's no pricing for this plan
         if hourly_price == 'N/A'
@@ -83,11 +100,11 @@ module ::EC2::Catalog
         spec = {
           id: instance_type,
           ram: memory,
-          cpu: vcpu,
-          disk: '',
+          cpu: cpus,
+          disk: disk,
           transfer: 'unlimited',
-          dollars_per_hr: hourly_price,
-          dollars_per_mo: ''
+          dollars_per_hr: '%.2f' % hourly_price,
+          dollars_per_mo: '%.2f' % monthly_price
         }
         
         # add the spec to the plan
@@ -98,9 +115,22 @@ module ::EC2::Catalog
       end
     end
     
-    # convert regions/plans from hashes to lists
+    # convert regions/plans from hashes to lists for the final catalog
+    catalog = []
     
-    dcs
+    dcs.each_pair do |_key, dc|
+      plans = []
+      
+      dc[:plans].each_pair do |_id, plan|
+        plans << plan
+      end
+      
+      dc[:plans] = plans
+      
+      catalog << dc
+    end
+    
+    ::File.write ASSET_FILE, catalog.to_json
   end
   
   protected
@@ -109,6 +139,75 @@ module ::EC2::Catalog
     conn = Faraday.new(:url => 'http://www.ec2instances.info')
     res = conn.get '/instances.json'
     JSON.parse res.body
+  end
+  
+  def self.ebs_prices
+    # So this is a bit of a trick... Since aws pricing is a nightmare to extract
+    # we're just going to pull the javascript (which is essentially JSON) that
+    # they load into their html price selector. We do have to strip some junk
+    # out of the document before we can parse the json though.
+    conn = Faraday.new(:url => 'https://a0.awsstatic.com')
+    res = conn.get '/pricing/1/ebs/pricing-ebs.js'
+    
+    data = JSON.parse(res.body.match(/callback\(\n(.*)\n\);/)[1])
+    
+    prices = {}
+    
+    data["config"]["regions"].each do |region|
+      monthly_price = region["types"].first["values"].first["prices"]["USD"].to_f
+      hourly_price = monthly_price / 720
+      
+      prices[region["region"]] = {
+        hourly: hourly_price,
+        monthly: monthly_price
+      }
+    end
+    
+    prices
+  end
+  
+  def self.calculate_disk(mbs)
+    # == Standard ==
+    # 512MB 20
+    # 1G    30
+    # 2G    40
+    # 4G    60
+    # 8G    80
+    # 16G   160
+    # 32G   320
+    # 48G   480
+    # 64G   640
+    # 
+    # == High RAM ==
+    # 16   30 1.875
+    # 32   90 2.8
+    # 64   200 
+    # 128  340
+    # 224  500
+    
+    # For now, let's keep it simple and just use the standard multiplier
+    # for all plans. Later, we can determine the slope equation for High RAM
+    # tiers. 
+    
+    gbs = mbs / 1024
+    
+    if gbs < 1
+      return 20
+    end
+    
+    if gbs < 2
+      return 30
+    end
+    
+    if gbs < 4
+      return 40
+    end
+    
+    if gbs < 8
+      return 60
+    end
+    
+    gbs * 10
   end
   
 end
